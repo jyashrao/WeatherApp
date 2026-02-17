3379a0e98c3c59039eadca35efd4a09738c90c6f
1010YASH <jyashrao10@gmail.com>
2026-02-17 17:58:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('kaha maiiiiiiiiii');
+
+const API_KEY="e26bcbf6f2b96d86d82e7d975c85c4ec";
+
+async function showWeather(){
+    let city="bikini";
+
+    const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+    const data=await response.json();
+    console.log("Whether data:"+ data);
+
+    let newPara=document.createElement('p');
+    newPara.textContent=`${data?.main?.temp.toFixed(2)} degree Celcius` 
+    document.body.appendChild(newPara);  
+}
+
